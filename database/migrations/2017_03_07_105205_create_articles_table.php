408a6f3eb8dc7a3ee20252dd6c676398f8deffce
Killian Blais <contact@killianblais.fr>
2017-03-12 18:10:49 +0100

    Basic Laravel app

---
+++ database/migrations/2017_03_07_105205_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->text('content');
+            $table->string('title')->default('hello');
+            $table->unsigned('author_id');
+            $table->datetime('published_at')->nullable();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
