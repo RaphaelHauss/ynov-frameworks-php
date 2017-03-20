7420f192eae0ec93cc68f875a0e4b3469cd79d17
Killian Blais <contact@killianblais.fr>
2017-03-20 11:40:28 +0100

    Add queue

--- app/Mail/UserRegistered.php
+++ app/Mail/UserRegistered.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class UserRegistered extends Mailable
+class UserRegistered extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
